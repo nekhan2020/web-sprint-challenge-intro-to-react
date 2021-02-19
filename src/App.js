be018512d96bc2cc4c0a514f95b9c5558fcc649d
Nadeem Khan <nadeemekhan@gmail.com>
2021-02-19 12:12:36 -0800

    mvp low still styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import { apiUrl } from "./APIURL/index";
+import styled from "styled-components";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const AppHolder = styled.div`
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+    flex-wrap: wrap;
+
+    h1 {
+      font-weight: 500;
+      font-size: 3rem;
+    }
+  `;
+
+  const [characters, setCharacters] = useState([]);
+
+  const getCharacters = () => {
+    axios
+      .get(`${apiUrl}people/`)
+      .then((res) => {
+        setCharacters(res.data);
+      })
+      .catch((error) => {
+        console.log(`Something went wrong: ${error}`);
+      });
+  };
+
+  useEffect(getCharacters, []);
+
   return (
-    <div className="App">
+    <AppHolder>
       <h1 className="Header">Characters</h1>
-    </div>
+      {characters.map((e) => {
+        return <Character props={e}></Character>;
+      })}
+    </AppHolder>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,42 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const CharacterDiv = styled.div`
+  border: 1px red solid;
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  /* flex-wrap: wrap; */
+  justify-content: center;
+  /* align-items: center; */
+
+  h5 {
+    text-align: center;
+  }
+
+  ul li {
+    list-style: none;
+  }
+`;
+
+const Character = ({ props }) => {
+  return (
+    <CharacterDiv>
+      <h2>{props.name}</h2>
+      <h4>Gender: {props.gender}</h4>
+      <h5>INFO:</h5>
+      <ul>
+        <li>Mass:{props.mass}</li>
+        <li>Birth year:{props.birth_year}</li>
+        {/* <li>Home World:{props.homeworld}</li> */}{" "}
+        {/*todo couldn't get an api
+        inside of an api look into how to do this later */}
+      </ul>
+    </CharacterDiv>
+  );
+};
+
+export default Character;
+
+//todo not sure why theres so much space in between INFO and the stats.. also isnt a perfect centering... look into fixing this and understanding it deeper later
